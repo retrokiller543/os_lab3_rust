f8a29935368db0e102e09cb6de67be8ed674fabc
Tosic.killer <emil.schutt@gmail.com>
2024-03-11 12:09:34 +0100

    fixed feature flag bug with abi3 mode

--- file_system/src/py_bindings/mod.rs
+++ file_system/src/py_bindings/mod.rs
@@ -43,40 +43,6 @@ impl From<PyErr> for IOHandlerError {
     }
 }
 
-/*
-pub fn setup_logger() -> Result<()> {
-    // Get the current timestamp
-    let now = Local::now();
-    // Format the timestamp as a string in the desired format
-    let timestamp = now.format("%Y-%m-%d_%H-%M-%S").to_string();
-    // Create the log filename with the timestamp
-    let log_filename = format!("logs/{}.log", timestamp);
-    // Create the log file and directory if needed
-    std::fs::create_dir_all("logs")?;
-
-    let file = File::create(log_filename)?;
-
-    Builder::from_env(Env::default().default_filter_or("info"))
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "[{} {} - {}:{}] {}",
-                Local::now().format("%Y-%m-%d %H:%M:%S"),
-                record.level(),
-                record
-                    .file()
-                    .unwrap_or(record.module_path().unwrap_or("unknown")),
-                record.line().unwrap_or(0),
-                record.args()
-            )
-        })
-        .target(env_logger::Target::Pipe(Box::new(file)))
-        .init();
-    Ok(())
-}
-*/
-
-
 macro_rules! logger_builder {
     ($lvl:expr) => {
         {
